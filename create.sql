b0ceaf7f2808c401a102d0294fa7ed2759744ee2
Zicheng Gao <kaynato@live.com>
2016-11-21 16:59:22 -0500

    final text fixes

--- create.sql
+++ create.sql
@@ -3,6 +3,7 @@ CREATE TABLE Cardholder(
 	address CHAR(100) NOT NULL,
 	contact NUMERIC(10,0) NOT NULL,
 	fine 	NUMERIC (5,2),
+	delinquent BOOLEAN,
 	PRIMARY KEY (card_id)
 );
 
@@ -38,33 +39,37 @@ CREATE TABLE Media(
 
 CREATE TABLE Book(
 	ISBN NUMERIC(13,0) NOT NULL, 
+	insta_no NUMERIC(3,0) NOT NULL,
 	no_pages NUMERIC(4,0),
-	PRIMARY KEY (ISBN),
-	FOREIGN KEY (ISBN) REFERENCES Media
+	PRIMARY KEY (ISBN, insta_no),
+	FOREIGN KEY (ISBN, insta_no) REFERENCES Media (ISBN, insta_no)
 );
 
 CREATE TABLE Video(
 	ISBN NUMERIC(13,0) NOT NULL, 
+	insta_no NUMERIC(3,0) NOT NULL, 	
 	length VARCHAR(10), 
 	director CHAR(100),
-	PRIMARY KEY (ISBN),
-	FOREIGN KEY (ISBN) REFERENCES Media
+	PRIMARY KEY (ISBN, insta_no),
+	FOREIGN KEY (ISBN, insta_no) REFERENCES Media (ISBN, insta_no)
 );
 
 CREATE TABLE Periodical(
 	ISBN 	NUMERIC(13,0) NOT NULL, 
+	insta_no NUMERIC(3,0) NOT NULL,
 	vol_no 	NUMERIC(4,0) NOT NULL, 	
 	issue_no 	NUMERIC(4,0) NOT NULL,
-	PRIMARY KEY (ISBN), 
-	FOREIGN KEY (ISBN) REFERENCES Book
+	PRIMARY KEY (ISBN, insta_no), 
+	FOREIGN KEY (ISBN, insta_no) REFERENCES Book (ISBN, insta_no)
 );
 
 CREATE TABLE Authored(
-	ISBN NUMERIC(13,0) NOT NULL, 
-	author_id VARCHAR(10,0) NOT NULL, 
-	PRIMARY KEY (ISBN, author_id),
-	FOREIGN KEY (ISBN) REFERENCES Media,
-	FOREIGN KEY (author_id) REFERENCES Author
+	ISBN NUMERIC(13,0) NOT NULL,
+	insta_no NUMERIC(3,0) NOT NULL,
+	author_id VARCHAR(10) NOT NULL,
+	PRIMARY KEY (ISBN, insta_no, author_id),
+	FOREIGN KEY (ISBN, insta_no) REFERENCES Media (ISBN, insta_no),
+	FOREIGN KEY (author_id) REFERENCES Authors (author_id)
 ); 
 
 CREATE TABLE Checkout(
@@ -72,11 +77,12 @@ CREATE TABLE Checkout(
 	insta_no NUMERIC(3,0) NOT NULL,
 	card_id VARCHAR(10) NOT NULL, 
 	dateout DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-	expiry DATETIME NOT NULL DEFAULT,
+	expiry DATETIME NOT NULL,
+	# should be system variable
 	renewals INT DEFAULT 0,
 	PRIMARY KEY (ISBN, insta_no, card_id),
-	FOREIGN KEY (ISBN, insta_no) REFERENCES Media,
-	FOREIGN KEY (card_id) REFERENCES Cardholder
+	FOREIGN KEY (ISBN, insta_no) REFERENCES Media (ISBN, insta_no),
+	FOREIGN KEY (card_id) REFERENCES Cardholder (card_id)
 );
 
 CREATE TABLE Reserve(
@@ -87,16 +93,17 @@ CREATE TABLE Reserve(
 	checkout DATE NOT NULL,
 	expiry DATE NOT NULL,
 	PRIMARY KEY (ISBN, insta_no, card_id),
-	FOREIGN KEY (ISBN, insta_no) REFERENCES Media,
-	FOREIGN KEY (card_id) REFERENCES Cardholder
+	FOREIGN KEY (ISBN, insta_no) REFERENCES Media (ISBN, insta_no),
+	FOREIGN KEY (card_id) REFERENCES Cardholder (card_id)
 );
 
 CREATE TABLE Loan(
-	loanlib VARCHAR(10) NOT NULL, 
-	borrlib VARCHAR(10) NOT NULL,
+	loanlib_id VARCHAR(10) NOT NULL, 
+	brwlib_id VARCHAR(10) NOT NULL,
 	ISBN NUMERIC(13,0) NOT NULL, 
 	insta_no NUMERIC(3,0) NOT NULL,
-	PRIMARY KEY (lib_id, lib_id, ISBN, insta_no),
-	FOREIGN KEY (loanlib_id, brwlib_id) REFERENCES Library,
-	FOREIGN KEY (ISBN, insta_no) REFERENCES Media
+	PRIMARY KEY (loanlib_id, brwlib_id, ISBN, insta_no),
+	FOREIGN KEY (loanlib_id) REFERENCES Library (lib_id),
+	FOREIGN KEY (brwlib_id) REFERENCES Library (lib_id),
+	FOREIGN KEY (ISBN, insta_no) REFERENCES Media (ISBN, insta_no)
 );
